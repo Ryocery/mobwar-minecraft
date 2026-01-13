8d2aae82e5ce345c1b655df71755cad17b97f219
Ryocery <youri.haentjens@gmail.com>
2026-01-13 15:13:09 +0100

    Refactor species enumeration and improve getSpecies method for clarity

--- src/main/java/com/ryocery/mobwar/mobs/Species.java
+++ src/main/java/com/ryocery/mobwar/mobs/Species.java
@@ -6,6 +6,7 @@ import net.minecraft.world.entity.animal.horse.Llama;
 import net.minecraft.world.entity.boss.wither.WitherBoss;
 import net.minecraft.world.entity.monster.*;
 import net.minecraft.world.entity.monster.breeze.Breeze;
+import net.minecraft.world.entity.monster.hoglin.Hoglin;
 import net.minecraft.world.entity.monster.piglin.AbstractPiglin;
 
 public enum Species {
@@ -26,46 +27,48 @@ public enum Species {
 
     public static Species getSpecies(LivingEntity entity) {
         return switch (entity) {
-            case Zombie           ignored -> ZOMBIES;
+            case Zombie             ignored -> ZOMBIES;
+            case Zoglin             ignored -> ZOMBIES;
 
-            case AbstractSkeleton ignored -> SKELETAL;
+            case AbstractSkeleton   ignored -> SKELETAL;
 
-            case Creeper          ignored -> CREEPERS;
+            case Creeper            ignored -> CREEPERS;
 
-            case Spider           ignored -> ARTHROPODS;
-            case Silverfish       ignored -> ARTHROPODS;
-            case Endermite        ignored -> ARTHROPODS;
-            case Bee              ignored -> ARTHROPODS;
+            case Spider             ignored -> ARTHROPODS;
+            case Silverfish         ignored -> ARTHROPODS;
+            case Endermite          ignored -> ARTHROPODS;
+            case Bee                ignored -> ARTHROPODS;
 
-            case AbstractIllager  ignored -> ILLAGERS;
-            case Ravager          ignored -> ILLAGERS;
-            case Vex              ignored -> ILLAGERS;
-            case Witch            ignored -> ILLAGERS;
+            case AbstractIllager    ignored -> ILLAGERS;
+            case Ravager            ignored -> ILLAGERS;
+            case Vex                ignored -> ILLAGERS;
+            case Witch              ignored -> ILLAGERS;
 
-            case EnderMan         ignored -> ENDER;
-            case Shulker          ignored -> ENDER;
+            case EnderMan           ignored -> ENDER;
+            case Shulker            ignored -> ENDER;
 
-            case AbstractPiglin   ignored -> PIGLINS;
+            case AbstractPiglin     ignored -> PIGLINS;
 
-            case IronGolem        ignored -> GOLEMS;
-            case SnowGolem        ignored -> GOLEMS;
+            case IronGolem          ignored -> GOLEMS;
+            case SnowGolem          ignored -> GOLEMS;
 
-            case Wolf             ignored -> BEASTS;
-            case Fox              ignored -> BEASTS;
-            case PolarBear        ignored -> BEASTS;
-            case Panda            ignored -> BEASTS;
-            case Llama            ignored -> BEASTS;
-            case Ocelot           ignored -> BEASTS;
+            case Wolf               ignored -> BEASTS;
+            case Fox                ignored -> BEASTS;
+            case PolarBear          ignored -> BEASTS;
+            case Panda              ignored -> BEASTS;
+            case Llama              ignored -> BEASTS;
+            case Ocelot             ignored -> BEASTS;
+            case Hoglin             ignored -> BEASTS;
 
-            case Guardian         ignored -> WATER_MOBS;
+            case Guardian           ignored -> WATER_MOBS;
 
-            case Slime            ignored -> SLIMES;
+            case Slime              ignored -> SLIMES;
 
-            case Blaze            ignored -> ELEMENTALS;
-            case Ghast            ignored -> ELEMENTALS;
-            case Breeze           ignored -> ELEMENTALS;
+            case Blaze              ignored -> ELEMENTALS;
+            case Ghast              ignored -> ELEMENTALS;
+            case Breeze             ignored -> ELEMENTALS;
 
-            case WitherBoss       ignored -> WITHER;
+            case WitherBoss         ignored -> WITHER;
 
             default -> UNKNOWN;
         };
